14a4802a32e671af04fdbeff885f2d9eab265288
milesmarchant <miles@marchant.net>
2015-05-21 22:52:58 -0700

    Made it not a list of functions at all

--- org/frcbitbucketbase/control/profile/Spline.java
+++ org/frcbitbucketbase/control/profile/Spline.java
@@ -1,22 +1,23 @@
 package org.frcbitbucketbase.control.profile;
 
+import java.util.List;
 import java.util.function.LongFunction;
 
 import org.frcbitbucketbase.control.MovementVector;
 
-public abstract class Spline{
+public class Spline{
 	
-	LongFunction<MovementVector>[] functions;
-	long startTime;
-	long endTime;
+	LongFunction<MovementVector> functions;
+	private long startTime;
+	private long endTime;
 	
-	public Spline(LongFunction<MovementVector>[] functions, long startTime, long endTime){
+	public Spline(LongFunction<MovementVector> functions, long startTime, long endTime){
 		this.functions = functions;
 		this.endTime = endTime;
 	}
 	
 	public MovementVector calculate(int order, long time){
-		return functions[order-1].apply(time);
+		return functions.apply(time);
 	}
 	
 	public long getStartTime(){
